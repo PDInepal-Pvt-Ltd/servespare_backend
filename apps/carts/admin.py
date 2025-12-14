cc937dbb200c0c2f3a48d7a657fc385ba6bd9ba6
aanishKumal06 <anishKumal0202@gmail.com>
2025-12-14 15:23:50 +0545

    feat: implement cart management functionality with models, serializers, views, and API documentation

---
+++ apps/carts/admin.py
@@ -0,0 +1,70 @@
+from django.contrib import admin
+from apps.carts.models import Cart, CartItem
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    """Admin interface for Cart model"""
+    list_display = ['id', 'user', 'total_items', 'subtotal', 'created', 'is_active']
+    list_filter = ['is_active', 'created', 'modified']
+    search_fields = ['user__username', 'user__email', 'user__full_name']
+    readonly_fields = ['created', 'modified', 'total_items', 'subtotal']
+    date_hierarchy = 'created'
+    
+    fieldsets = (
+        ('Cart Information', {
+            'fields': ('user', 'total_items', 'subtotal')
+        }),
+        ('Status', {
+            'fields': ('is_active', 'is_removed')
+        }),
+        ('Timestamps', {
+            'fields': ('created', 'modified'),
+            'classes': ('collapse',)
+        }),
+    )
+    
+    def has_add_permission(self, request):
+        """Disable manual cart creation in admin"""
+        return False
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    """Admin interface for CartItem model"""
+    list_display = [
+        'id',
+        'cart',
+        'inventory',
+        'quantity',
+        'price',
+        'total_price',
+        'created',
+        'is_active'
+    ]
+    list_filter = ['is_active', 'created', 'modified']
+    search_fields = [
+        'cart__user__username',
+        'inventory__item_name',
+        'inventory__part_number'
+    ]
+    readonly_fields = ['created', 'modified', 'total_price']
+    date_hierarchy = 'created'
+    
+    fieldsets = (
+        ('Cart Item Information', {
+            'fields': ('cart', 'inventory', 'quantity', 'price', 'total_price')
+        }),
+        ('Status', {
+            'fields': ('is_active', 'is_removed')
+        }),
+        ('Timestamps', {
+            'fields': ('created', 'modified'),
+            'classes': ('collapse',)
+        }),
+    )
+    
+    def has_add_permission(self, request):
+        """Disable manual cart item creation in admin"""
+        return False
+
---
+++ apps/carts/serializers/__init__.py
@@ -0,0 +1,8 @@
+from .cart_serializers import CartItemSerializer, CartSerializer, AddToCartSerializer, UpdateCartItemSerializer
+
+__all__ = [
+    'CartItemSerializer',
+    'CartSerializer',
+    'AddToCartSerializer',
+    'UpdateCartItemSerializer',
+]
