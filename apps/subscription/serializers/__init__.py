dae3e3e72b09e1ceebad471a90d05e0c80c06476
csubedi111 <chandansubedi111@gmail.com>
2025-12-22 14:24:07 +0545

    subscribe email

--- apps/subscription/serializers/__init__.py
+++ apps/subscription/serializers/__init__.py
@@ -1,6 +1,7 @@
 # Subscription serializers package
 from apps.subscription.serializers.subscription_plan import SubscriptionPlanSerializer
 from apps.subscription.serializers.subscription import SubscriptionSerializer
+from apps.subscription.serializers.subscriber_email import SubscriberEmailSerializer
 
-__all__ = ['SubscriptionPlanSerializer', 'SubscriptionSerializer']
+__all__ = ['SubscriptionPlanSerializer', 'SubscriptionSerializer', 'SubscriberEmailSerializer']
 
--- apps/subscription/views/__init__.py
+++ apps/subscription/views/__init__.py
@@ -1,6 +1,7 @@
 # Subscription views package
 from apps.subscription.views.subscription_plan import SubscriptionPlanViewSet
 from apps.subscription.views.subscription import SubscriptionViewSet
+from apps.subscription.views.subscriber_email import SubscriberEmailCreateView
 
-__all__ = ['SubscriptionPlanViewSet', 'SubscriptionViewSet']
+__all__ = ['SubscriptionPlanViewSet', 'SubscriptionViewSet', 'SubscriberEmailCreateView']
 
