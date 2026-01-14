2d903162a4b819c3f33fe33d788d0c2c329f6b58
csubedi111 <chandansubedi111@gmail.com>
2026-01-14 16:22:31 +0545

    update: database update to mysql

--- core/settings.py
+++ core/settings.py
@@ -127,24 +127,24 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
-# 'default': {
-# 'ENGINE': 'django.db.backends.mysql',
-# 'NAME': 'servesp1_servespare',
-# 'HOST': 'localhost',
-# 'PORT': '3306',
-# 'USER': 'servesp1_admin',
-# 'PASSWORD' :'github.com'
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+DATABASES = {
+'default': {
+'ENGINE': 'django.db.backends.mysql',
+'NAME': 'servesp1_servespare',
+'HOST': 'localhost',
+'PORT': '3306',
+'USER': 'servesp1_admin',
+'PASSWORD' :'github.com'
 
-# }
+}
     
-# }
+}
 
 
 # Password validation
