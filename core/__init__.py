677100b80793508f47128e5a22263157c3029f0d
csubedi111 <chandansubedi111@gmail.com>
2026-01-23 14:48:14 +0545

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,2 +1,8 @@
+# __init__.py
 import pymysql
+
+# Fake a high-enough version to satisfy Django's mysqlclient check (>=2.2.1)
+pymysql.version_info = (2, 2, 1, 'final', 0)
+
+# Make PyMySQL act as MySQLdb
 pymysql.install_as_MySQLdb()
