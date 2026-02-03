7c53b1e4d4341af23b78e5669ef582bd1d33d9a8
csubedi111 <chandansubedi111@gmail.com>
2026-02-03 12:18:20 +0545

    fix: Correct cheque type choices by replacing 'given' with 'received'

--- apps/cashandbank/models/cheque.py
+++ apps/cashandbank/models/cheque.py
@@ -5,10 +5,10 @@ from decimal import Decimal
 
 class Cheque(models.Model):
     CHEQUE_TYPE_ISSUED = 'issued'
-    CHEQUE_TYPE_GIVEN = 'given'
+    CHEQUE_TYPE_RECEIVED = 'received'
     CHEQUE_TYPE_CHOICES = [
         (CHEQUE_TYPE_ISSUED, 'Issued'),
-        (CHEQUE_TYPE_GIVEN, 'Given'),
+        (CHEQUE_TYPE_RECEIVED, 'Received'),
     ]
 
     REMINDER_SAME_DAY = '0'
